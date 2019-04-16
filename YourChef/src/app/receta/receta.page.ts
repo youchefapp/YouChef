21280e4c8ef7b7c1055ca3b410fc2e8b5789f94a
David Arroyo <daarroyo@ucm.es>
2019-04-16 10:06:20 +0200

    Update app name

---
+++ YourChef/src/app/receta/receta.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { RecetasService } from 'src/providers/recetas/recetas.service';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
+@Component({
+  selector: 'app-receta',
+  templateUrl: './receta.page.html',
+  styleUrls: ['./receta.page.scss'],
+})
+export class RecetaPage implements OnInit {
+  receta: any;
+
+  constructor(private recetasService: RecetasService, private iab: InAppBrowser) { }
+
+  ngOnInit() {
+    
+  }
+
+  ionViewWillEnter() {
+    this.receta = this.recetasService.selectedReceta;
+  }
+
+  visitAuthor() {
+    const browser = this.iab.create(this.receta.url, '_blank', 'location=yes');
+    browser.show();
+  }
+
+  round(n:number) {
+    return Math.round(n * 100) / 100;
+  }
+
+}
