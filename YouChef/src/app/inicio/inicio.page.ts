b4893adca30f762d010d8b844cbad2527522d70b
David Arroyo <daarroyo@ucm.es>
2019-04-16 10:08:11 +0200

    Remove old app

--- YouChef/src/app/inicio/inicio.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-inicio',
-  templateUrl: 'inicio.page.html',
-  styleUrls: ['inicio.page.scss']
-})
-export class InicioPage {}
--- YouChef/src/app/perfil/perfil.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-perfil',
-  templateUrl: 'perfil.page.html',
-  styleUrls: ['perfil.page.scss']
-})
-export class PerfilPage {}
